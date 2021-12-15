e2952b3e2accb08ddfb84d1c490c24120451a07a
rene <reneripardo@gmail.com>
2021-12-14 22:07:37 -0300

    update branch.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,9 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<any> {
+    //await this.appService.seed();
+    //return this.appService.getHello();
+    return this.appService.getEmployeeById(1);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -18,7 +18,55 @@ export class AppService {
     @InjectRepository(Task)
     private taskRepo: Repository<Task>
     ) {}
-  getHello(): string {
+    //employee 1 CEO
+    async seed() {
+      const ceo = this.employeeRepo.create({
+        name: 'Mr. CEO'
+      });
+      await this.employeeRepo.save(ceo);
+
+      const ceoContactInfo = this.contactInfoRepo.create({
+        email: 'test@email.com',
+      });
+      ceoContactInfo.employee = ceo;
+      await this.contactInfoRepo.save(ceoContactInfo);
+
+      // employee 2 manager (me)
+      const manager = this.employeeRepo.create({
+        name: 'Marcius',
+        manager: ceo,
+      });
+
+      const task1 = this.taskRepo.create({name: 'Hire people'});
+      await this.taskRepo.save(task1);
+      const task2 = this.taskRepo.create({name: 'Present to CEO'});
+      await this.taskRepo.save(task2);
+
+      manager.tasks = [task1, task2];
+
+      const meeting1 = this.meetingRepo.create({zoomUrl: 'meeting.com'});
+      meeting1.attendees = [ceo];
+      await this.meetingRepo.save(meeting1);
+
+      manager.meetings = [meeting1];
+
+      await this.employeeRepo.save(manager);
+    }
+
+    getEmployeeById(id: number) {
+      return this.employeeRepo.findOne(id, {
+        relations: [
+          'manager',
+          'directReports',
+          'tasks',
+          'contactInfo',
+          'meetings'
+        ]
+      })
+    }
+
+  
+    getHello(): string {
     return 'Hello World!';
   }
 }
